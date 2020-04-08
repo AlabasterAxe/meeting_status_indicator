8a4896348641f26e3e304ae62a5110ecb9a74f06
Matt <matthew.ed.keller@gmail.com>
2020-04-08 14:01:09 -0400

    new strategy for the chrome extension

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,45 +1,27 @@
 'use strict';
 
-const HANGOUTS_PREFIX = "https://hangouts.google.com/call/";
-const DEFAULT_SIGN_ENDPOINT = "http://192.168.1.22:5000";
+const HANGOUTS_PATTERN = 'https://hangouts.google.com/call/*';
+const DEFAULT_SIGN_ENDPOINT = 'http://192.168.1.22:5000';
 
-function handleRemovedTab(tabId, existingHangouts, signEndpoint) {
-  const tabIdx = existingHangouts.indexOf(tabId);
-  if (tabIdx >= 0) {
-    existingHangouts.splice(tabIdx)
-    if (existingHangouts.length === 0) {
-      fetch(signEndpoint + "/off?source=vc", {
-        mode: "no-cors"
-      });
-    }
-  }
-}
+function checkTabsAndUpdateStatusIfNecessary() {
+  chrome.storage.local.get(null, value => {
+    const committedStatus = value && value.committedStatus === 'boolean' ? value.committedStatus : null;
+    const signEndpoint = value && value.endpoint ? value.endpoint : DEFAULT_SIGN_ENDPOINT;
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-      chrome.storage.local.get(null, (value) => {
-        const existingHangouts = value ? value.hangouts || [] : [];
-        const signEndpoint = value ? value.endpoint || DEFAULT_SIGN_ENDPOINT : DEFAULT_SIGN_ENDPOINT;
-        if (changeInfo.url.startsWith(HANGOUTS_PREFIX)) {
-          if (existingHangouts.length === 0) {
-            fetch(signEndpoint + "/on?source=vc", {
-              mode: "no-cors"
-            });
-          }
-          existingHangouts.push(tabId);
-        } else {
-          handleRemovedTab(tabId, existingHangouts, signEndpoint);
-        }
-        chrome.storage.local.set({hangouts: Array.from(new Set(existingHangouts))});
-      });
-    }
-  });
-
-  chrome.tabs.onRemoved.addListener((tabId) => {
-    chrome.storage.local.get(null, (value) => {
-      const existingHangouts = value ? value.hangouts || [] : [];
-        const signEndpoint = value ? value.endpoint || DEFAULT_SIGN_ENDPOINT : DEFAULT_SIGN_ENDPOINT;
-      handleRemovedTab(tabId, existingHangouts, signEndpoint);
-      chrome.storage.local.set({hangouts: existingHangouts});
+    chrome.tabs.query({ url: HANGOUTS_PATTERN }, tabs => {
+      const currentStatus = tabs.length > 0;
+      if (currentStatus != committedStatus) {
+        const path = currentStatus ? 'on' : 'off';
+        fetch(`${signEndpoint}/${path}?source=vc`, {
+          mode: 'no-cors',
+        }).then(() => {
+          chrome.storage.local.set({ committedStatus: currentStatus });
+        });
+      }
     });
   });
+}
+
+chrome.tabs.onUpdated.addListener(checkTabsAndUpdateStatusIfNecessary);
+
+chrome.tabs.onRemoved.addListener(checkTabsAndUpdateStatusIfNecessary);
